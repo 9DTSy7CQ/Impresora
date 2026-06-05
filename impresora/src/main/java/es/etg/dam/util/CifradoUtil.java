665c673310ba726e4911247fa5276712e56056d0
9DTSy7CQ <alconralf3@gmail.com>
2026-06-05 16:17:56 +0200

    Correción código hardcode

--- impresora/src/main/java/es/etg/dam/util/CifradoUtil.java
+++ impresora/src/main/java/es/etg/dam/util/CifradoUtil.java
@@ -12,16 +12,17 @@ public class CifradoUtil {
     private static final String CODIFICACION = "UTF-8";
     private static final int LONGITUD_CLAVE = 16;
     public static final String PASS = "1234567890123456";
+    private static final int POSICION_CLAVE = 0;
 
     public static String cifrar(String mensaje, String pass) throws Exception {
-        Key key = new SecretKeySpec(pass.getBytes(CODIFICACION), 0, LONGITUD_CLAVE, ALGORITMO);
+        Key key = new SecretKeySpec(pass.getBytes(CODIFICACION), POSICION_CLAVE, LONGITUD_CLAVE, ALGORITMO);
         Cipher aes = Cipher.getInstance(MODO);
         aes.init(Cipher.ENCRYPT_MODE, key);
         return Base64.getEncoder().encodeToString(aes.doFinal(mensaje.getBytes(CODIFICACION)));
     }
 
     public static String descifrar(String mensaje, String pass) throws Exception {
-        Key key = new SecretKeySpec(pass.getBytes(CODIFICACION), 0, LONGITUD_CLAVE, ALGORITMO);
+        Key key = new SecretKeySpec(pass.getBytes(CODIFICACION), POSICION_CLAVE, LONGITUD_CLAVE, ALGORITMO);
         Cipher aes = Cipher.getInstance(MODO);
         aes.init(Cipher.DECRYPT_MODE, key);
         return new String(aes.doFinal(Base64.getDecoder().decode(mensaje)), CODIFICACION);
